233605640d84212a943266a7f4872079c24e3156
OmicronPersei <wheeler.weise@gmail.com>
2017-02-20 19:36:03 -0600

    Initial commit of the boiler plate.

---
+++ Javascript.js
@@ -0,0 +1,10 @@
+/*global $, jQuery, navigator*/
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, white: true  */
+
+$(document).ready(function() {
+  "use strict";
+  
+ 
+  
+  
+});
