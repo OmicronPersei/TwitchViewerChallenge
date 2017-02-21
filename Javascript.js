64157d57b94ed8a3f07753c8e0546c11b9514b63
OmicronPersei <wheeler.weise@gmail.com>
2017-02-20 20:25:01 -0600

    Initial commit towards twitch API.  Does not yet work :(

--- Javascript.js
+++ Javascript.js
@@ -1,6 +1,36 @@
 /*global $, jQuery, navigator*/
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, white: true  */
 
+function TwitchUserInfoAPI(userName, mainInfoReadyCallback) {
+  "use strict";
+  
+  var apiURL = "https://wind-bow.gomix.me/twitch-api";
+  var getUserInfoStr = "/users/" + userName;
+  this.UserInfo = undefined;
+  
+  $.ajax( {
+    dataType: "jsonp",
+    url: this.apiURL + this.getUserInfoStr,
+    data: null,
+    success: function(obj) {
+      this.UserInfo = obj;
+      
+      if (mainInfoReadyCallback !== undefined) {
+        mainInfoReadyCallback();
+      }
+    }
+  });
+  
+  
+}
+
+function TwitchUserDisplay(twitchUserInfoAPIObj) {
+  "use strict";
+  
+  
+  
+}
+
 $(document).ready(function() {
   "use strict";
   
