32dcc3e20327dfdafe2c6b1469d2215506b14b8a
OmicronPersei <wheeler.weise@gmail.com>
2017-02-22 19:40:21 -0600

    Can now successfully get twitch user info.

--- Javascript.js
+++ Javascript.js
@@ -1,28 +1,78 @@
 /*global $, jQuery, navigator*/
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, white: true  */
 
-function TwitchUserInfoAPI(userName, mainInfoReadyCallback) {
+function TwitchUserInfoAPI(userName) {
+  "use strict";
+  
+  this.UserName = userName;
+  
+  this.UserInfo = undefined;
+  this.ChannelInfo = undefined;
+  this.StreamInfo = undefined;
+}
+
+TwitchUserInfoAPI.prototype.isAllDataReady = function() {
+  "use strict";
+  
+  return ((this.UserInfo !== undefined)  &&
+          (this.ChannelInfo !== undefined) &&
+          (this.StreamInfo !== undefined));
+};
+
+TwitchUserInfoAPI.prototype.getUserInfo = function(allInfoReadyCallback) {
   "use strict";
   
   var apiURL = "https://wind-bow.gomix.me/twitch-api";
-  var getUserInfoStr = "/users/" + userName;
-  this.UserInfo = undefined;
+  
+  var getUserInfoStr = "/users/" + this.UserName;
+  
+  var oThis = this;
   
   $.ajax( {
     dataType: "jsonp",
-    url: this.apiURL + this.getUserInfoStr,
+    url: apiURL + getUserInfoStr,
     data: null,
     success: function(obj) {
-      this.UserInfo = obj;
+      oThis.UserInfo = obj;
       
-      if (mainInfoReadyCallback !== undefined) {
-        mainInfoReadyCallback();
+      if (oThis.isAllDataReady()) {
+        allInfoReadyCallback();
+      }
+    }
+  });
+  
+  var getStreamInfoStr = "/streams/" + this.UserName;
+  
+  $.ajax( {
+    dataType: "jsonp",
+    url: apiURL + getStreamInfoStr,
+    data: null,
+    success: function(obj) {
+      oThis.StreamInfo = obj;
+      
+      if (oThis.isAllDataReady()) {
+        allInfoReadyCallback();
+      }
+    }
+  });
+  
+  var getChannelInfoStr = "/channels/" + this.UserName;
+  
+  $.ajax( {
+    dataType: "jsonp",
+    url: apiURL + getChannelInfoStr,
+    data: null,
+    success: function(obj) {
+      oThis.ChannelInfo = obj;
+      
+      if (oThis.isAllDataReady()) {
+        allInfoReadyCallback();
       }
     }
   });
   
   
-}
+};
 
 function TwitchUserDisplay(twitchUserInfoAPIObj) {
   "use strict";
